fc53cbc674b62e2e060cd82268d74774d77fb039
widmogrod <widmogrod@916e42d6-b34b-0410-ba52-c53f5bb2f993>
2008-04-25 22:30:20 +0000

    Poznawanie ruby - kilka przykładów

--- lib/main_callbacks.rb
+++ lib/main_callbacks.rb
@@ -28,3 +28,20 @@ end
 def on_zamknij3_clicked()
 	$tree
 end
+
+def on_dodajGospodarstwo_clicked()
+	print "function 'on_dodajGospodarstwo_clicked' not implemented\n"
+end
+def on_listaGospodarstw_clicked()
+	print "function 'on_listaGospodarstw_clicked' not implemented\n"
+end
+def on_zamknij_clicked()
+	print "function 'on_zamknij_clicked' not implemented\n"
+end
+def on_about_activate()
+	print "function 'on_about_activate' not implemented\n"
+end
+
+def on_zapisz_listaGospodarstw_clicked()
+	print "function 'on_zapisz_listaGospodarstw_clicked' not implemented\n"
+end
