c020e8831c572205617e9009f6618790b5ad7e3e
widmogrod <widmogrod@916e42d6-b34b-0410-ba52-c53f5bb2f993>
2008-04-26 22:10:11 +0000

    poprawione polaczenie z sqlite3

--- trunk/lib/sqlite.rb
+++ trunk/lib/sqlite.rb
@@ -1,5 +1,8 @@
 
-require 'sqlite'
+require 'sqlite3'
 
-db = SQLite::Database.new( "test.db" )
-rows = db.execute("select * from test" )
+db = SQLite3::Database.new("database.sqlite")
+db.execute( "select * from uboj" ) do |row|
+  p row
+end
+db.close
