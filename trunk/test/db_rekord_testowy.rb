4f5e7a4d45b2d72a9ba2bf9c72e52d502e1d4632
widmogrod <widmogrod@916e42d6-b34b-0410-ba52-c53f5bb2f993>
2008-04-28 02:17:06 +0000

    test manipulacji danymi z BD + dodanie DB::REcord::ostatioDodaneId

---
+++ trunk/test/db_rekord_testowy.rb
@@ -0,0 +1,36 @@
+# 
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+ 
+
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+
+require 'test/unit'
+require 'db'
+
+class RekordTestowy < Db::Record
+  def init
+    @id = 'id_kategorii'
+    @tabela = 'kategoria_bydla'
+    @kolumny = ['id_kategorii','nazwa']
+  end
+end
+
+class TestDbRekordTestowy < Test::Unit::TestCase
+  def setup
+    @polaczenie = SQLite3::Database.new("database.sqlite")
+  end
+
+  def test_a
+    rekord = RekordTestowy.new
+
+    assert          rekord.dodaj({'nazwa' => 'Krowa'}), 'Rekord nie zostal dodany'
+    
+    lastId = rekord.ostatioDodaneId
+
+    assert_nil      rekord.wczytaj(1000), 'Nieistniejacy rekord zostal znaleziony'
+    assert_not_nil  rekord.wczytaj(lastId), 'Znaleziony rekord nie jest prawidlowy'
+    assert          rekord.usun(lastId), 'Rekord nie zostal uniety'
+    assert_nil      rekord.wczytaj(lastId), 'Nieistniejacy rekord zostal znaleziony'
+  end
+end
