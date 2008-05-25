a3c68ea9ae6d4db55d5266d434758ee142b183b2
widmogrod <widmogrod@916e42d6-b34b-0410-ba52-c53f5bb2f993>
2008-05-25 21:51:45 +0000

    Stworzenie dodatkowych katalogow models, views, actions. Poczatek przepisywania kodu

---
+++ lib/views/glade/main_callbacks.rb
@@ -0,0 +1,45 @@
+
+def init_callbacks(xmltree)
+	$tree = xmltree
+	puts "initialize"
+end
+
+
+def on_gospodarstwa1_activate()
+	print "function 'on_gospodarstwa1_activate' not implemented\n"
+end
+def on_zakoncz1_activate()
+	print "function 'on_zakoncz1_activate' not implemented\n"
+end
+def on_dodajGospodarstwo_clicked()
+	print "function 'on_dodajGospodarstwo_clicked' not implemented\n"
+end
+def on_zapisz_listaGospodarstw_clicked()
+	print "function 'on_zapisz_listaGospodarstw_clicked' not implemented\n"
+end
+def on_partie1_activate()
+	print "function 'on_partie1_activate' not implemented\n"
+end
+def on_zapiszToolButton_clicked()
+	print "function 'on_zapiszToolButton_clicked' not implemented\n"
+end
+def on_listaGospodarstw_clicked()
+	print "function 'on_listaGospodarstw_clicked' not implemented\n"
+end
+def on_zamknij_clicked()
+	print "function 'on_zamknij_clicked' not implemented\n"
+end
+def on_uboje1_activate()
+	print "function 'on_uboje1_activate' not implemented\n"
+end
+def on_about_activate()
+	print "function 'on_about_activate' not implemented\n"
+end
+
+
+
+
+
+
+
+
