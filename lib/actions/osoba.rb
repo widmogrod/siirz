e3e4b0dac04da5cdcc3d2ba20010e1b43002793b
widmogrod <widmogrod@916e42d6-b34b-0410-ba52-c53f5bb2f993>
2008-05-25 21:47:48 +0000

    Stworzenie dodatkowych katalogow models, views, actions. Poczatek przepisywania kodu

---
+++ lib/actions/osoba.rb
@@ -0,0 +1,11 @@
+# 
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+ 
+module Actions
+  class Osoba < Action
+    def dodaj()
+      put 'wykonuja Osoba::dodaj'
+    end
+  end
+end
